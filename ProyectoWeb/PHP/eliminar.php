c5819455a1097fffc4a19b47509f28e3135b8518
Gis <carpinteiro.gisel@gmail.com>
2019-11-20 18:04:47 -0600

    Se elimina a un mono

--- ProyectoWeb/PHP/eliminar.php
+++ ProyectoWeb/PHP/eliminar.php
@@ -2,11 +2,14 @@
 
     if(isset($_POST['btnEliminar'])){
         $idUsuario = $_POST['btnEliminar'];
+        $con = mysqli_connect("localhost", "root", "", "helados") or die ("Error");
+        $consultaEliminar = "update trabajador set eliminado=1 where id_trabajador=".$idUsuario;
+        $resultado = mysqli_query($con, $consultaEliminar);
         
-
+        if($resultado){
+        //update trabajador set eliminado=1 where id_trabajador=1;
+        echo "Se elimino al usuario con id ".$idUsuario;
+        }
     }
-
-
-
 
 ?>
