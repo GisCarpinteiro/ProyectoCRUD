deae05571053327b87e509f35804b2b3ad4754c8
Gis <carpinteiro.gisel@gmail.com>
2019-11-22 10:04:04 -0600

    Algunos cambios

--- ProyectoWeb/PHP/index.php
+++ ProyectoWeb/PHP/index.php
@@ -6,18 +6,34 @@
             if(isset($_POST['btnEnviar'])){
                 $usuario = $_POST['inputId'];
                 $contrasena = $_POST['inputContra'];
-                echo "us".$usuario."contra".$contrasena;
+                //echo "us".$usuario."contra".$contrasena;
                 $consulta = "select *from trabajador where id_trabajador=".$usuario." and contrasena='".$contrasena."'";
                 //$consulta = "select *from trabajador where id_trabajador=".$usuario;
                 $ejecutar = mysqli_query($con, $consulta);
                 
                 //echo get_resource_type($ejecutar) . "\n";          
                 if(mysqli_num_rows($ejecutar)){
-                    echo"Bienvenido";
-                    echo "<script>alert('Bienvenido');</script>";
+                    $mostrarNombre = "select nombre from trabajador where id_trabajador=".$usuario;
+                    $resMostrarN = mysqli_query($con, $mostrarNombre) or die(mysqli_error($con));
+                    while($filas = mysqli_fetch_assoc($resMostrarN)){
+                        
+                            //  echo'<script type="text/javascript">
+                            //         alert("Bienvenido "'.$filas["nombre"].');
+                            //         window.location.href="trabajador.php";
+                            //  </script>';
+                            echo'<script type="text/javascript">
+                                     alert("Bienvenido ");
+                                     window.location.href="trabajador.php";
+                              </script>';
+                            //echo "ok ".$filas['nombre'];
+                    }
 
                 }
-                else
-                    echo"No existe tu cuenta";
+                else{
+                    echo'<script type="text/javascript">
+                    alert("No existe tu cuenta");
+                    window.location.href="../index.html";
+                    </script>';
+                }
             }
 ?>
